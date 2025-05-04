ea6ebe957777e159223da94aea17ef7719f58c10
Iva Djondovic <ivadj03@gmail.com>
2025-05-04 18:03:49 +0200

    Latest changes

--- main.py
+++ main.py
@@ -14,54 +14,89 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    response = requests.get("https://api.exconvert.com/currencies", params={"access_key":api_key})
-    if response.status_code == requests.status_codes.codes.ok:
-        data = response.json()
-        currencies = data.get("currencies", {})
-    else:
-        currencies = {}
-    return templates.TemplateResponse("index.html", {"request": request, "items": currencies.keys()})
+    currencies={}
+    try:
+        response = requests.get("https://api.exconvert.com/currencies", params={"access_key":api_key})
+        if response.status_code == requests.status_codes.codes.ok:
+            data = response.json()
+            currencies = data.get("currencies", {})
+            error = None
+        else:
+            error = response.text
+    except Exception as e:
+        error = e
+    return templates.TemplateResponse("index.html", {"request": request, "items": currencies.keys() if currencies else [], "error": error })
     
 @app.post("/", response_class=HTMLResponse)
 async def post_form(request: Request, from_currency: str = Form(...), to_currency: str = Form(...), amount: str = Form(...)):
-    response = requests.get("https://api.exconvert.com/currencies", {"access_key":api_key})
-    if response.status_code == requests.status_codes.codes.ok:
-        data = response.json()
-        currencies = data.get("currencies", {})
-    else:
-        currencies = {}
-
-    conversion = requests.get("https://api.exconvert.com/convert", {"access_key":api_key, "from":from_currency, "to":to_currency, "amount":amount})
-    if conversion.status_code == requests.status_codes.codes.ok:
-        data = conversion.json()
-        result = data.get("result", {}).get(to_currency, "/")
-    else:
-        result = "/"
-    return templates.TemplateResponse("index.html", {"request": request, "items": currencies.keys(), "result": result, "from_currency": from_currency, "to_currency": to_currency, "amount": amount})
+    currencies = {}
+    try:    
+        response = requests.get("https://api.exconvert.com/currencies", {"access_key":api_key})
+        if response.status_code == requests.status_codes.codes.ok:
+            data = response.json()
+            currencies = data.get("currencies", {})
+            error = None
+        else:
+            error = response.text
+        conversion = requests.get("https://api.exconvert.com/convert", {"access_key":api_key, "from":from_currency, "to":to_currency, "amount":amount})
+        if conversion.status_code == requests.status_codes.codes.ok:
+            data = conversion.json()
+            result = data.get("result", {}).get(to_currency, "/")
+        else:
+            result = ""
+            error = conversion.text
+    except Exception as e:
+        result = ""
+        error = e
+    return templates.TemplateResponse("index.html", {"request": request, "items": currencies.keys() if currencies else [], "result": round(result,2) if result else "", "from_currency": from_currency, "to_currency": to_currency, "amount": amount, "error": error})
 
 @app.get("/exchange_rates", response_class=HTMLResponse)
 async def exchange_rates(request: Request):
-    response = requests.get("https://api.exconvert.com/currencies", params={"access_key":api_key})
-    if response.status_code == requests.status_codes.codes.ok:
-        data = response.json()
-        currencies = data.get("currencies", {})
-    else:
+    try:
+        response = requests.get("https://api.exconvert.com/currencies", params={"access_key":api_key})
+        if response.status_code == requests.status_codes.codes.ok:
+            data = response.json()
+            currencies = data.get("currencies", {})
+            error = None
+        else:
+            currencies = {}
+            error = response.text
+    except Exception as e:
         currencies = {}
-    return templates.TemplateResponse("exchange_rates.html", {"request": request, "items": currencies.keys()})
+        error = e
+    return templates.TemplateResponse("exchange_rates.html", {"request": request, "items": currencies.keys() if currencies else [], "type_of_currency": "physical", "error": error })   
 
 @app.post("/exchange_rates", response_class=HTMLResponse)
-async def post_exchange_rates(request: Request, from_currency: str = Form(...)):
-    response = requests.get("https://api.exconvert.com/currencies", params={"access_key":api_key})
-    if response.status_code == requests.status_codes.codes.ok:
-        data = response.json()
-        currencies = data.get("currencies", {})
+async def post_exchange_rates(request: Request, trigger: str=Form(...), type_of_currency: str = Form(...), from_currency: str = Form(None)):
+    if trigger == "radio":
+        from_currency = None
+    if type_of_currency == "physical":
+        endpoint = "https://api.exconvert.com/currencies"
     else:
-        currencies = {}
+        endpoint = "https://api.exconvert.com/crypto/currencies"
+    currencies = {}
+    rates = {}
+    try:
+        response = requests.get(endpoint, params={"access_key":api_key})
+        if response.status_code == requests.status_codes.codes.ok:
+            data = response.json()
+            currencies = data.get("currencies", {})
+            error = None
+        else:
+            error = response.text
 
-    result = requests.get("https://api.exconvert.com/fetchAll", params={"access_key":api_key, "from":from_currency})
-    if result.status_code == requests.status_codes.codes.ok:
-        data = result.json()
-        rates = data.get("result", {})
-    else:
-        rates = {}
-    return templates.TemplateResponse("exchange_rates.html", {"request": request, "items":currencies.keys(), "from_currency": from_currency, "rates": rates})
+        if from_currency:
+            if type_of_currency == "physical":
+                endpoint = "https://api.exconvert.com/fetchAll"
+            else:
+                endpoint = "https://api.exconvert.com/crypto/fetchAll"
+            result = requests.get(endpoint, params={"access_key":api_key, "from":from_currency})
+            if result.status_code == requests.status_codes.codes.ok:
+                data = result.json()
+                rates = data.get("result", {})
+            else:
+                rates = {}
+                error = result.text
+    except Exception as e:
+        error = e
+    return templates.TemplateResponse("exchange_rates.html", {"request": request, "items": currencies.keys(), "type_of_currency": type_of_currency, "from_currency": from_currency, "rates": rates, "error": error})
